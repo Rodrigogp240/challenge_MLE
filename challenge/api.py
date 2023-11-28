5313922a1505189432613a1934e9372ac340fa35
Rodrigogp240 <rodrigogp240@gmail.com>
2023-11-28 20:46:44 -0300

    Add funcionality to the API

--- challenge/api.py
+++ challenge/api.py
@@ -1,6 +1,18 @@
 import fastapi
 
+from pydantic import BaseModel,list
+from .model import DelayModel
+
 app = fastapi.FastAPI()
+model = DelayModel()
+
+class FlightItem(BaseModel):
+    OPERA: str
+    TIPOVUELO: str
+    MES: int
+
+class FlightData(BaseModel):
+    flights: List[FlightItem]
 
 @app.get("/health", status_code=200)
 async def get_health() -> dict:
@@ -9,5 +21,11 @@ async def get_health() -> dict:
     }
 
 @app.post("/predict", status_code=200)
-async def post_predict() -> dict:
-    return
+async def post_predict(data: FlightData) -> dict:
+    predictions = []
+
+    for flight_item in data.flights:
+        prediction_result = model.predict(flight_item.OPERA, flight_item.TIPOVUELO, flight_item.MES)
+        predictions.append({"prediction": prediction_result})
+
+    return {"predictions": predictions}
