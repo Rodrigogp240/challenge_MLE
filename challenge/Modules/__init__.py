092cb9d226251f1fb9d65c56258888ccc4001535
Rodrigogp240 <rodrigogp240@gmail.com>
2023-11-27 22:13:19 -0300

    Added Helper module

---
+++ challenge/Modules/__init__.py
@@ -0,0 +1,5 @@
+__all__ = ["getMinDiff","isHighSeason","getPeriodDay"]
+
+from .getMinDiff import get_min_diff
+from .isHighSeason import is_high_season
+from .getPeriodDay import get_period_day
---
+++ challenge/Modules/getPeriodDay.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+def get_period_day(date):
+    date_time = datetime.strptime(date, '%Y-%m-%d %H:%M:%S').time()
+    morning_min = datetime.strptime("05:00", '%H:%M').time()
+    morning_max = datetime.strptime("11:59", '%H:%M').time()
+    afternoon_min = datetime.strptime("12:00", '%H:%M').time()
+    afternoon_max = datetime.strptime("18:59", '%H:%M').time()
+    evening_min = datetime.strptime("19:00", '%H:%M').time()
+    evening_max = datetime.strptime("23:59", '%H:%M').time()
+    night_min = datetime.strptime("00:00", '%H:%M').time()
+    night_max = datetime.strptime("4:59", '%H:%M').time()
+    
+    if(date_time > morning_min and date_time < morning_max):
+        return 'mañana'
+    elif(date_time > afternoon_min and date_time < afternoon_max):
+        return 'tarde'
+    elif(
+        (date_time > evening_min and date_time < evening_max) or
+        (date_time > night_min and date_time < night_max)
+    ):
+        return 'noche'
