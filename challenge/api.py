dae54b7a48c9faea496e72b223d1b1476da9a830
Rodrigogp240 <rodrigogp240@gmail.com>
2023-11-29 22:45:14 -0300

    working of API

--- challenge/api.py
+++ challenge/api.py
@@ -2,10 +2,10 @@ import fastapi
 
 from pydantic import BaseModel
 from typing import List
-from .model import DelayModel
+
 
 app = fastapi.FastAPI()
-model = DelayModel()
+
 
 class FlightItem(BaseModel):
     OPERA: str
@@ -24,9 +24,4 @@ async def get_health() -> dict:
 @app.post("/predict", status_code=200)
 async def post_predict(data: FlightData) -> dict:
     predictions = []
-
-    for flight_item in data.flights:
-        prediction_result = model.predict(flight_item.OPERA, flight_item.TIPOVUELO, flight_item.MES)
-        predictions.append({"prediction": prediction_result})
-
     return {"predictions": predictions}
