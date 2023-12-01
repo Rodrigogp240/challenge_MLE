47243b67f32f386b397e09e261dbbad0d6c768fb
Rodrigogp240 <rodrigogp240@gmail.com>
2023-12-01 10:37:34 -0300

    add Doc to importe method

--- challenge/model.py
+++ challenge/model.py
@@ -63,7 +63,7 @@ class DelayModel:
 
         if not target_column:
             return features
-        
+
         target = data[target_column]
         return (features, target.to_frame())
 
@@ -79,10 +79,11 @@ class DelayModel:
             features (pd.DataFrame): preprocessed data.
             target (pd.DataFrame): target.
         """
-        x_train, _, y_train, _ = train_test_split(features, target, test_size = 0.33, random_state = 42)
+        x_train, _, y_train, _ = train_test_split(
+            features, target, test_size=0.33, random_state=42)
         n_y0 = 37298
         n_y1 = 8400
-        class_weight={1: n_y0/len(y_train), 0: n_y1/len(y_train)}
+        class_weight = {1: n_y0/len(y_train), 0: n_y1/len(y_train)}
         reg_model_2 = LogisticRegression(class_weight=class_weight)
         self._model = reg_model_2.fit(x_train, y_train)
         return None
@@ -102,7 +103,8 @@ class DelayModel:
         """
         if self._model is None:
             # the probles is with the test
-            raise ValueError("Model has not been trained. Call 'fit' method first.")
+            raise ValueError(
+                "Model has not been trained. Call 'fit' method first.")
 
         predictions = self._model.predict(features)
 
@@ -110,14 +112,23 @@ class DelayModel:
         predicted_targets = list(map(int, predictions))
 
         return predicted_targets
-    
+
     def importer(
         self,
-    )-> None:
+    ) -> None:
+        """
+        Save the trained logistic regression model.
+
+        Raises:
+            ValueError: If the model is not trained (call 'fit' method first).
+
+        This method serializes and saves the trained logistic regression model to
+        'trained_log_reg-0.1.0.pkl'. Ensure the model is trained before using this.
+        """
         if self._model is None:
             # the probles is with the test
-            raise ValueError("Model has not been trained. Call 'fit' method first.")
-        with open('trained_log_reg-0.1.0.pkl',"wb") as f:
-            pickle.dump(self._model,f)
+            raise ValueError(
+                "Model has not been trained. Call 'fit' method first.")
+        with open('trained_log_reg-0.1.0.pkl', "wb") as f:
+            pickle.dump(self._model, f)
         return None
-
