a22a0da3007c97d49cc29d117ff717bfe3e996d0
Rodrigogp240 <rodrigogp240@gmail.com>
2023-12-01 12:47:44 -0300

    delate unused code

--- challenge/trained_model.py
+++
@@ -1,9 +0,0 @@
-import pickle
-import re
-from pathlib import Path
-
-
-BASED_DIR = Path(__file__).resolve(strict=True).parent
-
-with open("f{BASED_DIR}/challenge/trained_log_reg-0.1.0.pkl","rb") as f:
-    model = pickle.load(f)
